ef710f06940484394fe05f8b184f98b6b9aae29f
Frederick Jaime <f.jaime@bigspaceship.com>
2018-12-13 18:06:23 -0500

    added sassCompile task with sourcemaps and autoprefixer, nano

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,43 @@
 
 const { series, parallel, src, dest, watch } = require('gulp');
+const sass  = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('autoprefixer');
 const config = require('./.phoenix/config');
+const cssnano = require('cssnano');
+
 
 
 
 function build(cb) {
   cb();
-  console.log(config.gulp );
+  console.log(config.css.scssDir );
  // console.log(config.pkg.title);
+}
+
+let sassCompile = function(cb) {
+
+  let plugins = [
+    autoprefixer({browsers: ['last 2 version']}),
+    cssnano()
+  ];
+
+  return src([`${config.css.scssDir}/sxm.phoenix.scss`])
+    .pipe(sourcemaps.init())
+    .pipe(postcss(plugins))
+    .pipe(sass().on('error',sass.logError))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest(config.css.distDir));
+
 }
 
 
 
 exports.build = build;
 exports.default = series(
-  build
-  );
+  build,
+  sassCompile
+);
 
 
